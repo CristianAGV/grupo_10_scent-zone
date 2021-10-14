b6c1644f2c4c7c1bfbb6557c5cb9df366120fe84
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-10-13 21:42:32 -0500

    basic MVC structure

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, './public/')))
+
+/* En este caso aca se serviria el home */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+
+
+
+app.listen(port, () => console.log("Servidor corriendo en el puerto", port))
