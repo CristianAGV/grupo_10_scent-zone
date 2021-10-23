201510bb83ebdd1a1b0083d50def0d509b73074b
Salma <salma.morales.hoyos@hotmail.com>
2021-10-23 12:26:26 -0500

    add footer partials and inlude ejs

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -8,6 +8,9 @@ const controller = {
     questions: (req, res) => {
         res.render('questions')
     },
+    registro: (req, res) => {
+        res.render('registro')
+    },
 }
 
 module.exports = controller
--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -5,6 +5,7 @@ const mainController = require('../controllers/main.controller')
 router.get('/', mainController.home)
 router.get('/cart', mainController.cart)
 router.get('/questions', mainController.questions)
+router.get('/registro', mainController.registro)
 
 
 module.exports = router
