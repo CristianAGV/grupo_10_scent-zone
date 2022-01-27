35a924c92e956b9aa7e1ea6aac0de058303522e5
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2022-01-27 17:24:52 -0500

    fixing db config file

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,15 +6,15 @@ module.exports ={
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOSTNAME,
-    "port": process.env.PORT,
-    "dialect": "mysql"
+    "port": process.env.DB_PORT,
+    "dialect": "mysql"    
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOSTNAME,
-    "port": process.env.PORT,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "production": {
@@ -22,7 +22,7 @@ module.exports ={
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOSTNAME,
-    "port": process.env.PORT,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   }
 }
