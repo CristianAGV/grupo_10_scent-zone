b7a0b06f498ed3099e7cf14c1c06480e459465de
Cristian G <cristianandresgv03@gmail.com>
2022-02-21 18:38:13 -0500

    fixing orders error and displaying each user's order in cart

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,30 +1,36 @@
 //const productsModel = require('../model/index')
-const productsModel = require('../model/productsModel')
-const {validationResult} = require('express-validator')
+const productsModel = require("../model/productsModel");
+const ordersModel = require("../model/ordersModel");
+const { validationResult } = require("express-validator");
 const showProducts = productsModel.showProducts();
 
-
 const mainController = {
-    home: async (req, res) => {
-        try {
-            let products = await productsModel.showProducts()
-            res.render('home', {products, user: req.session.userLogged })
-        } catch (error) {
-            res.send(error)
-        }
-        
-    },
-    cart: (req, res) => {
-        res.render('cart', {user: req.session.userLogged})
-    },
-    questions: (req, res) => {
-        res.render('questions')
-    },
-    errorCatch: (req, res) => {
-        res.render('comeBack')
+  home: async (req, res) => {
+    try {
+      let products = await productsModel.showProducts();
+      res.render("home", { products, user: req.session.userLogged });
+    } catch (error) {
+      res.send(error);
     }
-    
-
-}
+  },
+  cart: async (req, res) => {
+    try {
+      let userId = Number(req.session.userLogged.id);
+      let productsOfUser = await ordersModel.getUserOrders(userId);
+      res.render("cart", {
+        user: req.session.userLogged,
+        products: productsOfUser,
+      });
+    } catch (error) {
+      res.render("comeback");
+    }
+  },
+  questions: (req, res) => {
+    res.render("questions");
+  },
+  errorCatch: (req, res) => {
+    res.render("comeBack");
+  },
+};
 
 module.exports = mainController;
--- src/model/ordersModel.js
+++ src/model/ordersModel.js
@@ -1,14 +1,33 @@
-const db = require('../database/models');
+const db = require("../database/models");
 
 const ordersModel = {
-    createOrder: async(order)=>{
-        try {
-            let createdOrder = await db.orders.create(order);
-            console.log(createdOrder);
-        } catch (error) {
-            console.error(error);
-        }
+  createOrder: async (order) => {
+    try {
+      let createdOrder = await db.orders.create(order);
+      console.log(createdOrder);
+    } catch (error) {
+      console.error(error);
     }
-}
+  },
 
-module.exports = ordersModel
+  getUserOrders: async (id) => {
+    try {
+      let ordersInDatabase = await db.orders.findAll({
+        where: { user_id: id },
+      });
+      let idOfProducts = ordersInDatabase.map((order) => order.product_id);
+      const transformedProducts = await Promise.all(
+        idOfProducts.map(async (id) => {
+          return await db.products.findByPk(id);
+        })
+      );
+      return transformedProducts;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+};
+
+// ordersModel.getUserOrders(11);
+
+module.exports = ordersModel;
