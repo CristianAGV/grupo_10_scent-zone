105b7aeeedd126d89eb042be44e0e79ba60f3e96
Salma <salma.morales.hoyos@hotmail.com>
2022-02-27 13:50:16 -0500

    fix react card style

--- scentzone-dashboard/src/components/card.jsx
+++ scentzone-dashboard/src/components/card.jsx
@@ -6,6 +6,7 @@ function Card(props) {
         <div className="dashboard-card" id="dash-card">
             <h2>{props.total}</h2>
             <p>{props.object} in database</p>
+            
         </div>
         
     )
