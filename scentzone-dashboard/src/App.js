8610b7fee934b5ca0ed0a3a84a2d98cf9bb69acd
Edward Orozco <edwardorozco09@hotmail.com>
2022-03-01 03:48:57 -0500

    Categories React Component created and added to React App.js

--- scentzone-dashboard/src/App.js
+++ scentzone-dashboard/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import TotalsContainer from "./components/totalsContainer";
 import LastProduct from "./components/LastProduct";
 import ProductList from "./components/ProductList";
+import Categories from "./components/Categories";
+
 function App() {
   return (
     <div className="App">
       <h1>Scent Zone dashboard</h1>
       <TotalsContainer />
+      <Categories/>
       <h2>Products List</h2>
       <LastProduct />
       <ProductList/>
---
+++ scentzone-dashboard/src/components/Categories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function categories() {
+    return;
+}
+
+export default categories;
